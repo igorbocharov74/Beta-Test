d10d7c0fb06139dc021b5571a15bc4ee8175d99d
gleb-bocharov <gleb4151@gmail.com>
2016-07-20 23:51:54 +0300

    Markup
    Markup creating

---
+++ html/test.js
@@ -0,0 +1,21 @@
+  function startTimer() {
+    var my_timer = document.getElementById("my_timer");
+    var time = my_timer.innerHTML;
+    var arr = time.split(":");
+    var m = arr[0];
+    var s = arr[1];
+    if (s == 0) {
+      if (m == 0) {
+          alert("Время вышло");
+          window.location.reload();
+          return;
+      }
+      m--;
+      if (m < 10) m = "0" + m;
+      s = 59;
+    }
+    else s--;
+    if (s < 10) s = "0" + s;
+    document.getElementById("my_timer").innerHTML = m+":"+s;
+    setTimeout(startTimer, 1000);
+  }
