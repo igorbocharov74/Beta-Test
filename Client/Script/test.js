6339ec3a221fb5c0bca2b61d9c030ad0c499f5b9
Igor Bocharov <ibocharov5@gmail.com>
2016-07-24 22:16:08 +0300

    Knockout, Bootsrap
    Knockout, Bootsrap added

--- Client/Script/test.js
+++ Client/Script/test.js
@@ -1,21 +1,84 @@
-  function startTimer() {
-    var my_timer = document.getElementById("my_timer");
-    var time = my_timer.innerHTML;
-    var arr = time.split(":");
-    var m = arr[0];
-    var s = arr[1];
-    if (s == 0) {
-      if (m == 0) {
-          alert("Время вышло");
-          window.location.reload();
-          return;
-      }
-      m--;
-      if (m < 10) m = "0" + m;
-      s = 59;
-    }
-    else s--;
-    if (s < 10) s = "0" + s;
-    document.getElementById("my_timer").innerHTML = m+":"+s;
-    setTimeout(startTimer, 1000);
+$(document).ready(function(){
+  ko.applyBindings(new QuizViewModel(d));
+});
+
+function AnswerViewModel(answer) {
+  var self = this;
+  self.answer = answer;
+  self.selected = ko.observable(false);
+  return self;
+};
+  
+function TicketViewModel(question, answers) {
+  var self = this;
+  self.question = question;
+  self.answers = [];
+  for (var i = 0; i < answers.length; i++) {
+      self.answers.push(new AnswerViewModel(answers[i]));
   }
+  
+  //TODO: fix logic
+  self.select = function(answer) {
+      self.answers.forEach(function(a) { a.selected(false); });
+      answer.selected(true);
+  }
+  return self;
+};
+
+function QuizViewModel(data) {
+    var self = this;
+    
+    self.settings = data.quiz.settings;
+    
+    // Questions
+    self.correctAnswers = data.quiz.correctAnswers;
+    
+    self.tickets = ko.observableArray(data.quiz.tickets);
+    self.currentTicketId = ko.observable(0);
+    self.currentTicketIdNormalized = ko.computed(function(){return self.currentTicketId() + 1;});
+    self.currentTicket = ko.computed(function(){return self.tickets()[self.currentTicketId()];});
+
+    self.next = function() {self.currentTicketId(self.currentTicketId()++);}
+    self.prev = function() {self.currentTicketId(self.currentTicketId()--);}
+
+    // Timer
+    self.remainingMin = ko.observable(self.settings.timeLimit.min);
+    self.remainingMinFormatted = ko.computed(
+      function(){
+        return (self.remainingMin() < 10) ? "0" + self.remainingMin() : self.remainingMin();
+      }
+    )
+    self.remainingSec = ko.observable(self.settings.timeLimit.sec);
+    self.remainingSecFormatted = ko.computed(
+      function(){
+        return (self.remainingSec() < 10) ? "0" + self.remainingSec() : self.remainingSec();
+      }
+    )   
+    self.isTimerRunning = ko.observable(false);
+    
+    self.StartTimer = function(){
+      self.isTimerRunning(true);
+      self.timerId = window.setInterval(function(){
+        var min = self.remainingMin();
+        var sec = self.remainingSec();
+        if (sec === 0) {
+          if (min === 0) {
+            clearInterval(self.timerId);
+            self.isTimerRunning(false);
+            self.onTimeOver();
+            return;
+          }
+          self.remainingMin(--min);
+          self.remainingSec(59);
+        }
+        else self.remainingSec(--sec);
+      }, 1000)
+    }
+    
+    self.onTimeOver = function(){alert("That's all!");}
+    
+    //Start timer
+    self.StartTimer();
+    
+    return self;
+};
